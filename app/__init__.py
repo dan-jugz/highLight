40381a48f14ec54750f5859272b72c28212ba7cf
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 17:12:20 +0300

    install flask bootstrap and initialised it

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import DevConfig
-
+from flask_bootstrap import Bootstrap
 
 app =Flask(__name__,instance_relative_config=True)
 
@@ -8,5 +8,7 @@ app =Flask(__name__,instance_relative_config=True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+#Intiliazing Flask extensions
+bootstrap=Bootstrap(app)
 
 from app import views 
