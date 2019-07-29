1a8414a67741d59edd71a6ee56cd38d7f3cedc64
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 19:16:58 +0300

    add the function that makes an api request for an article

--- app/request.py
+++ app/request.py
@@ -10,6 +10,9 @@ api_key=app.config['NEWS_API_KEY']
 #Getting the news source base url
 sources_base_url=app.config['NEWS_SOURCES_BASE_URL']
 
+#Getting a news source articles
+articles_base_url=app.config['NEW_ARTICLES_BASE_URL']
+
 def get_sources(category):
     '''
     Function that gets the json response to our url request
@@ -52,4 +55,25 @@ def process_results(sources_list):
         source_obj=Source(id,name,desc,url,category,language,country)
         sources_results.append(source_obj)
 
-    return sources_results         
+   
+    return sources_results 
+
+def get_articles(id):
+    '''
+    Function that fetches a single news source and returns the source's articles
+    '''
+
+    get_articles_url=articles_base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data=url.read()
+        get_articles_response=json.loads(get_articles_data)
+
+        articles_results=None
+
+        if get_articles_response['articles']:
+            articles_result_list=get_articles_response['articles']
+            articles_results=process_article_results(articles_result_list)
+
+    return articles_results        
+        
