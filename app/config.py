ede195ba80752a652daa771a91029c27cf4f0ecf
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 16:15:28 +0300

    update the news sources to filter by category

--- app/config.py
+++ app/config.py
@@ -1,14 +1,27 @@
 class Config:
+    '''
+    General Configuration parent class
+    '''
 
-    NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?/category={}apiKey={}'
     NEWS_HEADLINES_BASE_URL='https://newsapi.org/v2/top-headlines?sources{}&apiKey={}'
     NEW_ARTICLES_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
 class ProdConfig(Config):
+    '''
+    Production Configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
     pass
 
 
 class DevConfig(Config):
+    '''
+    Development Configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
     DEBUG = True
 
 config_options = {
