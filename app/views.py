ff1595b312818a9bea123f52d822b9ca7994194f
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 15:44:23 +0300

    parse the source list to display various news sources

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 # Views
 @app.route('/')
@@ -9,12 +10,18 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+
+    #Getting the news sources
+    news_sources = get_sources()
+    print(news_sources)
+
     title = 'NEWS HIGHLIGHTS APP '
     return render_template('index.html',title = title)
+    
     
 @app.route("/newswatch/<source_id>")
 def news_source(source_id):
     '''
     View new_source page function that returns a news source page and its data
     '''
-    return render_template('newsSource.html',id =source_id)
+        return render_template('index.html',title=title,source=news_sources)
