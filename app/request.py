e5be7f18cb7ec6fcba0370da863dedc40e1a6c00
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 15:14:31 +0300

    add a get sources function that makes an api call to sources and fetches json data

--- app/request.py
+++ app/request.py
@@ -8,4 +8,23 @@ Source = source.Source
 api_key=app.config['NEWS_API_KEY']
 
 #Getting the news source base url
-sources_base_url=app.config['NEWS_SOURCES_BASE_URL'
+sources_base_url=app.config['NEWS_SOURCES_BASE_URL']
+
+def get_sources():
+    '''
+    Function that gets the json response to our url request
+    '''
+
+    get_sources_url=sources_base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data=url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results=None
+
+        if get_sources_response['sources']:
+            sources_result_list=get_sources_response['sources']
+            sources_results = process_results(sources_result_list)
+
+    return sources_results        
