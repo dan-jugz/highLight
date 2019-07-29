e9132b986b01b89a17462f52e73f3ae0ec441b5f
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 15:22:39 +0300

    add a function to process results into an object list

--- app/request.py
+++ app/request.py
@@ -27,4 +27,29 @@ def get_sources():
             sources_result_list=get_sources_response['sources']
             sources_results = process_results(sources_result_list)
 
-    return sources_results        
+    return sources_results  
+
+
+def process_results(sources_list):
+    '''
+    Function that processes the source_result_list and transforms them to a list of objects
+    Args:
+        sources_list:A list of dictionaries that contain news sources 
+     Returns:
+        sources_results:A list of new source objects   
+    '''
+
+    sources_results=[]
+    for source_item in sources_list:
+        id=source_item.get('id')
+        name=source_item.get('name')
+        desc=source_item.get('description')
+        url=source_item.get('url')
+        category=source_item.get('category')
+        language=source_item.get('language')
+        country=source_item.get('country')
+
+        source_obj=Source(id,name,desc,url,category,language,country)
+        sources_results.append(source_obj)
+
+    return sources_results         
