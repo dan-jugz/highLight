75eac0039ae26e0c65c3c8f9e43f361ae20649e0
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-29 14:31:20 +0300

    add the dynamic endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 
 
-app =Flask(__name__,instance_relative_config=true)
+app =Flask(__name__,instance_relative_config=True)
 
 #Setting up the configuration
 app.config.from_object(DevConfig)
--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 class Config:
-    pass
+
+    NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_HEADLINES_BASE_URL='https://newsapi.org/v2/top-headlines?sources{}&apiKey={}'
+    NEW_ARTICLES_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
 class ProdConfig(Config):
     pass
