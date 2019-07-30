1c79989103319ee09deec56290057c560757256f
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-30 07:05:45 +0300

    add the errors module to the init module

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
 
+#initialising app
 app =Flask(__name__,instance_relative_config=True)
 
 #Setting up the configuration
@@ -11,4 +12,5 @@ app.config.from_pyfile('config.py')
 #Intiliazing Flask extensions
 bootstrap=Bootstrap(app)
 
-from app import views 
+from app import views
+from app import errors
